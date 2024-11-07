84c0d21a5fa65de4e2926dcf2dc7eeae40a3b11d
Borg But Not Collective, ya know? <73506637+DrBorg@users.noreply.github.com>
2024-11-07 09:02:10 -0600

    Update UnpinTaskbarIcons.ps1

--- UnpinTaskbarIcons.ps1
+++ UnpinTaskbarIcons.ps1
@@ -3,14 +3,11 @@
 #================================
 
 # Remove all files inside the Taskbar APPDATA folder
-#================================
   Remove-Item -Path "$env:APPDATA\Microsoft\Internet Explorer\Quick Launch\User Pinned\TaskBar\*" -Force -Recurse -ErrorAction SilentlyContinue
 
 # Remove the Taskband Registry Key to delete taskbar data for recent apps.
-#================================
   Remove-Item -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Taskband" -Force -Recurse -ErrorAction SilentlyContinue
 
 #Start-Stop the File Explorer to refresh the taskbar.
-#================================
   Stop-Process -ProcessName explorer -Force
   Start-Process explorer
